8aac1dd87ce36b6ef190c1acf32b3a833c8672e7
Arman Asgharpoor <a.asgharpoor1993@gmail.com>
2026-05-22 18:03:28 +0300

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://ginkgoq.github.io",
